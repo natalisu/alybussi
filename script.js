290d349d69ce72d736c8ba96c88100c7051433df
Natalia Sulkama <natalisu@metropolia.fi>
2017-11-01 12:20:22 +0200

    Pohja: kartta & kello, uutisvirta

---
+++ script.js
@@ -0,0 +1,39 @@
+// KELLO
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('klo').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }; // add zero in front of numbers < 10
+    return i;
+}
+
+window.onload = function () {
+    startTime();
+}
+
+// UUTISVIRTA
+
+$('.uvirta').marquee({
+    //speed in milliseconds of the marquee
+    duration: 10000,
+    //gap in pixels between the tickers
+    // gap: 100,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: false
+});
