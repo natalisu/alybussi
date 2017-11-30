691f20fd6d4680937230c640f5e326999f0ab730
Vombatti <juha.selonen@outlook.com>
2017-11-30 11:53:00 +0200

    Päivitetty tiedosto

--- arduino/alybussi.py
+++ arduino/alybussi.py
@@ -1,5 +1,4 @@
 # Tämä sovellus pitää olla käynnissä Rasberry Pi että näppäimistö toimii älybussi sovelluksessa.
-#print("Hei Maailma")
 # Code for Raspberry PI
 from pykeyboard import PyKeyboard
 import time
@@ -8,15 +7,8 @@ import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
 keyboard = PyKeyboard()
 while 1:
-    #ser.readline()
-    #print (ser.readline())
-    ser.read()
-    #print (ser.read())
-    var1 = ser.read()
-    print (var1[0])
-    var2 = (var1[0] - 48)
-    print (var2)
-    string = str(var2)
-    keyboard.press_key(string)
-    #sleep(1)
-    keyboard.release_key(string)
+    button_value = ser.read()
+    button_key = (button_value[0] - 48)
+    key_str = str(button_key)
+    keyboard.press_key(key_str)
+    keyboard.release_key(key_str)
