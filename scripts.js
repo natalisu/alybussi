14dd1593162c88e54a35aeaa2bb4a45bcbbc071f
Natalia Sulkama <natalisu@metropolia.fi>
2017-12-10 16:06:03 +0200

    Kommentit javascriptiin

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
-// CLOCK FUNCTIONS
-
-// Seeing as you're already using moment, you can use that for all your 
-// date formatting needs. You can also use setInterval rather than setTimeout
+// KELLO
 var clock = document.getElementById('klo');
 
 setInterval(function () {
@@ -9,7 +6,9 @@ setInterval(function () {
   clock.textContent = time;
 }, 500);
 
-// SCREEN SAVER AND MODAL TIMEOUTS
+
+
+// NÄYTÖNSÄÄSTÄJÄ, MODAL TIMEOUTIT
 
 $("#naytons").click(function () {
   $("#naytons").fadeOut(800);
@@ -17,10 +16,7 @@ $("#naytons").click(function () {
 
 var idleTime = 0;
 $(document).ready(function () {
-  //Increment the idle time counter every minute.
-  var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
-
-  //Zero the idle timer on mouse movement.
+  var idleInterval = setInterval(timerIncrement, 60000);
   $(this).mousemove(function (e) {
     idleTime = 0;
   });
@@ -31,9 +27,8 @@ $(document).ready(function () {
 
 function timerIncrement() {
   idleTime = idleTime + 1;
-  if (idleTime > 5) {
-    $("#naytons").fadeIn(800);
-    // Jos näyttö ollut inaktiivisena 3 min, näytönsäästäjä palautuu
+  if (idleTime > 4) {
+    $("#naytons").fadeIn(800); // Jos näyttö ollut inaktiivisena 5 min, näytönsäästäjä palautuu
   }
 };
 
@@ -55,7 +50,10 @@ $('#nappi3').click(function () {
   }, 3 * 60 * 1000);
 });
 
-// NEWS MARQUEE
+
+
+
+// UUTISVIRTA JA KIELIASETUS
 
 function uutisVirta() {
   $('.uvirta').marquee({
@@ -96,8 +94,6 @@ function getNewsFeed(urlSetting) {
         var news = edge.node.title.replace(/&quot;/g, '\"');
         var text = document.createTextNode(news);
         fragment.appendChild(text);
-
-        // This is nasty. You should be adding a span with your content and styling the span, not forcing in a lot of nbsp
         fragment.appendChild(document.createTextNode("\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0"));
       });
 
@@ -107,7 +103,10 @@ function getNewsFeed(urlSetting) {
   );
 };
 
-// LANGUAGE BUTTON FUNCTIONS
+
+
+
+// KIELEN VAIHTO
 
 var langbtn = document.getElementById("langbtn");
 
@@ -183,28 +182,24 @@ $(document).ready(function () {
 
 });
 
+
+
 // GOOGLE SHEETS
-/* Here is where I access my sheets database, I would want to use it so it's easier for the customer to input values.
-I only need it to store 3 things: 
-- "sijainti" variable (current location, two values: latitude & longitude)
-- "locations" variable (marks bus stops on the map, three values: name, lat & lon)
-- Polyline markers (an array of locations that draw a line on the map, serves as the bus route, two values: lat & lon)
+// sijainti (lat & lon), locations (name, lat & lon), polyline markkerit (lat & lon)
 
-I'll mark each variable that I'd want to switch to the database in the code later on
-*/
-
-// With fetch, you need to format the response data, we want json, so we're calling the 
-// appropriate method here and returning it
 function getJson(response) {
   return response.json();
 }
 
-// @TODO: Handle errors in here
 function handleError(error) {
   console.error(error);
 }
 
-// GOOGLE MAPS, basically a horrible bunch of map functions
+
+
+
+// GOOGLE MAPS -funktiot
+
 var map;
 var infowindow;
 
@@ -227,7 +222,7 @@ function createMap(sheetData) {
     fullscreenControl: false
   });
 
-  // Second thing I would like to switch to the database. These mark the bus stops on the map
+  // Nämä Google Sheetsiin
   var locations = [
       ['VALIMO', 60.183431, 24.828609, ],
       ['ACRE', 60.182013, 24.830982],
@@ -235,7 +230,7 @@ function createMap(sheetData) {
     ];
 
   var request = {
-    location: sijainti, // Also here I need to access sijainti
+    location: sijainti,
     types: ['airport', 'amusement_park', 'aquarium', 'art-gallery', 'atm', 'bakery', 'bank', 'bar', 'beauty-salon', 'book_store', 'bowling_alley', 'cafe', 'cemetery', 'church', 'city_hall', 'clothing_store', 'convenience_store', 'dentist', 'department_store', 'doctor', 'electronics_store', 'florist', 'furniture-store', 'gas_station', 'gym', 'hair_care', 'hardware_store', 'home_goods_store', 'hospital', 'jewelry_store', 'laundry', 'library', 'liquor_store', 'lodging', 'meal_delivery', 'meal-takeaway', 'movie_rental', 'movie_theater', 'museum', 'night-club', 'park', 'pet_store', 'pharmacy', 'police', 'post_office', 'restaurant', 'school', 'shoe_store', 'shopping_mall', 'spa', 'stadium', 'store', 'university', 'zoo'],
     radius: '600'
   };
@@ -243,7 +238,7 @@ function createMap(sheetData) {
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, getServices);
 
-  // Looks up details for each of the generated services on the map
+  // Tiedot kartan palveluille
   function getPlaceDetails(place, status, title) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       var fragment = document.createDocumentFragment();
@@ -251,18 +246,18 @@ function createMap(sheetData) {
       var container = document.createElement('div');
       container.classList.add('services-info');
 
-      // Service names
+      // Hakee palveluiden nimet
       var title = document.createElement('h3');
       title.classList.add('service-title');
       title.textContent = place.name;
       container.appendChild(title);
 
-      // Service addresses
+      // Hakee palveluiden osoitteet
       var address = document.createElement('p');
       address.classList.add('service-address');
       address.textContent = place.formatted_address;
 
-      // Creates a container for the QR code
+      // Generoi QR-koodin
       var qrcode = document.createElement('img');
       qrcode.classList.add("qrcode");
       qrcode.setAttribute("id", place.id);
@@ -278,7 +273,6 @@ function createMap(sheetData) {
       var isOpen = document.createElement('p');
       isOpen.classList.add('is-open');
 
-      // Checks if the places are open duh
       function checkOpeningHours() {
         list.innerHTML = "";
         isOpen.innerHTML = "";
@@ -357,7 +351,7 @@ function createMap(sheetData) {
 
   var marker, i;
 
-  // Polyline markers, here is where I need to use "locations"
+  // Polyline markerit
   for (i = 0; i < locations.length; i++) {
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
@@ -372,7 +366,7 @@ function createMap(sheetData) {
       }
     });
 
-    // InfoWindow content, also using "locations"
+    // InfoWindow content
     var content = locations[i][0];
 
     var infowindow = new google.maps.InfoWindow({
@@ -406,7 +400,7 @@ function createMap(sheetData) {
     scale: 4
   };
 
-  // Creates the polyline, these coordinates I would want in the database as well
+  // Polyline koordinaatit, nämä Google Sheetsiin
   var line = new google.maps.Polyline({
     path: [{
         lat: 60.183341,
@@ -465,11 +459,13 @@ function createMap(sheetData) {
     map: map
   });
 
-  // BUS STOP QUERY
+
+
+
+  // BUSSIPYSÄKKIEN HAKU
+
   function getStops() {
     document.getElementById("busText").innerHTML = '';
-
-    // Here I need to use "sijainti" as well to search the bus stops based on current location
     var query = JSON.stringify({
       query: '{ stopsByRadius(lat: ' + sijainti.lat + ', lon: ' + sijainti.lng + ', radius: 500) { edges { node { stop { name, lat, lon, stoptimesForPatterns(numberOfDepartures: 1) { pattern { name } stoptimes { scheduledDeparture } } } } } } }'
     });
@@ -485,13 +481,12 @@ function createMap(sheetData) {
         var fragment = document.createDocumentFragment();
         var results = pysakit.data.stopsByRadius.edges.forEach(
           function (edge) {
-            // Sijainti here as well
             var start_lat = sijainti.lat;
             var start_lon = sijainti.lng;
             var lat = edge.node.stop.lat;
             var lon = edge.node.stop.lon;
 
-            // Calculates distance from current location to the bus stop
+            // Laskee etäisyyden nykyisestä sijainnista bussipysäkille
             function distance(lat1, lon1, lat2, lon2) {
               var p = 0.017453292519943295;
               var c = Math.cos;
@@ -508,7 +503,7 @@ function createMap(sheetData) {
             container.classList.add('stop');
             var br = document.createElement('br');
 
-            // Bus stop names
+            // Hakee pysäkkien nimet
             var title = document.createElement('h3');
             title.classList.add('stop-title')
             title.textContent = edge.node.stop.name + " (" + distance(start_lat, start_lon, lat, lon) + "m)";
@@ -517,7 +512,7 @@ function createMap(sheetData) {
             var list = document.createElement('ul');
             list.classList.add('stop-list');
 
-            // Filters bus schedules more than 15 minutes away from current time
+            // Filteröi tulokset ja hakee vain bussilinjat, jotka kulkevat seuraavan 15 minuutin sisään
             edge.node.stop.stoptimesForPatterns.filter(function (item) {
               var today = new Date();
               var currentSeconds = today.getSeconds() + (60 * today.getMinutes()) + (60 * 60 * today.getHours());
@@ -526,8 +521,7 @@ function createMap(sheetData) {
               return diff <= 900;
             }).map(function (item) {
               var time = moment.utc(item.stoptimes[0].scheduledDeparture * 1000).format('HH:mm');
-              var name = item.pattern.name.slice(0, -13).replace(' to ', ' ').replace(' (HSL:2222209)', '').replace(' (HSL:2215225)', '').replace(', tulo', '');
-
+              var name = item.pattern.name.slice(0, -13).replace(' to ', ' ');
               return {
                 time: time,
                 name: name,
@@ -585,7 +579,7 @@ function createMarker(place) {
   });
 }
 
-// We use fetch because it's cool and lets use use promises
+// Google Sheets -tietokantahaku
 fetch("https://sheets.googleapis.com/v4/spreadsheets/1oOiSqkOp_WEj7f21opfQzqpozwEHTb-K9YES7p-Ib8g/values/A3%3AB3?fields=values&key=AIzaSyAsKit0QXl6tApOMluYh8do3jaKrwfyUxs")
   .then(getJson)
   .then(createMap)
@@ -621,7 +615,7 @@ $(document).ready(function () {
 
 
 
-// ARDUINO BUTTONS, ignore plz
+// ARDUINO -FUNKTIOT
 
 document.addEventListener("keyup", function (event) {
   event.preventDefault();
